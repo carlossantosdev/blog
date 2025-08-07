f5f21d7c78c7a434501922d1836426968c172fa1
Carlos Santos <carlos.santos.dev@gmail.com>
2025-08-07 08:29:46 -0300

    feat: update hero section images

--- resources/views/components/hero-section/images.blade.php
+++ resources/views/components/hero-section/images.blade.php
@@ -1,36 +1,37 @@
 <div class="mt-14 flex justify-end gap-8 sm:-mt-32 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
-    <div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-0 xl:pt-80">
+    {{-- <div class="ml-auto w-56 flex-none space-y-8 pt-24 sm:pt-48 order-1 lg:pt-36 xl:order-0 xl:pt-48"> --}}
+    <div class=" w-56 flex-none space-y-8 pt-24 sm:pt-48 order-0 sm:order-0">
         <div class="relative">
-            <img src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                alt="" class="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+            <img src="{{ asset('images/hero-section/Carlos-Santos.png') }}" alt=""
+                class="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
             <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-gray-900/10 ring-inset">
             </div>
         </div>
     </div>
-    <div class="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+    <div class="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36 md:order-0">
         <div class="relative">
-            <img src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                alt="" class="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+            <img src="{{ asset('images/hero-section/glenth-logo.png') }}" alt=""
+                class="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
             <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-gray-900/10 ring-inset">
             </div>
         </div>
         <div class="relative">
-            <img src="https://webandcrafts.com/_next/image?url=https%3A%2F%2Fadmin.wac.co%2Fuploads%2FReasons_To_Use_Laravel_in_2025_df79d4b566.jpg&w=4500&q=90"
-                alt="" class="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+            <img src="{{ asset('images/hero-section/laravel-bg.png') }}" alt=""
+                class="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
             <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-gray-900/10 ring-inset">
             </div>
         </div>
     </div>
-    <div class="w-44 flex-none space-y-8 pt-32 sm:pt-10 opacity-50 lg:opacity-100">
+    <div class="w-44 flex-none space-y-8 pt-32 sm:pt-32 opacity-75 lg:opacity-100 md:pt-40 lg:pt-24">
         <div class="relative">
-            <img src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
-                alt="" class="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+            <img src="{{ asset('images/hero-section/ai-bg.webp') }}" alt=""
+                class="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
             <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-gray-900/10 ring-inset">
             </div>
         </div>
         <div class="relative">
-            <img src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                alt="" class="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+            <img src="{{ asset('images/hero-section/php-bg.png') }}" alt=""
+                class="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
             <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-gray-900/10 ring-inset">
             </div>
         </div>
