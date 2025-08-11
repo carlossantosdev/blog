b5cc9a9a5be39d55c4bae5046c584f0e96efa4a8
Carlos Santos <carlos.santos.dev@gmail.com>
2025-08-11 06:30:32 -0300

    feat: update footer content

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,17 +1,15 @@
 <div {{ $attributes->class('bg-gray-100') }}>
     <footer class="container py-8 lg:max-w-(--breakpoint-md) *:[&_a]:underline *:[&_a]:font-medium">
-        <nav class="grid grid-cols-2 gap-y-2 gap-x-6 sm:grid-cols-5 md:grid-cols-6 sm:place-items-center">
+        <nav class="flex flex-col md:flex-row items-center justify-center gap-4">
             <a wire:navigate href="{{ route('home') }}">Home</a>
             <a wire:navigate href="{{ route('blog.posts.index') }}">Articles</a>
             <a wire:navigate href="{{ route('blog.categories.index') }}">Categories</a>
-            <a href="{{ route('home') }}#about">About</a>
+            <a href="{{ route('about') }}">About</a>
             <a href="mailto:carlos.santos.dev@gmail.com">Contact</a>
         </nav>
 
-        {{-- <p class="mt-8 text-center text-balance">
-            My blog is hosted on <a href="{{ route('merchants.show', 'digitalocean') }}"
-                target="_blank">DigitalOcean</a>. Analytics provided by <a
-                href="{{ route('merchants.show', 'pirsch-analytics') }}" target="_blank">Pirsch</a>.
-        </p> --}}
+        <p class="mt-8 text-center text-balance">
+            carlossantosdev.com © 2025
+        </p>
     </footer>
 </div>
